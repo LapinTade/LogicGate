be976a0a608c01ae5f4927741f437c2d67d8d531
LapinTade <lapintade@RabbitDebian.(none)>
2013-01-25 17:30:28 +0100

    Ajout class gate et le main

---
+++ Gate.py
@@ -0,0 +1,28 @@
+"""
+@author: MARIN Jordy, ROZE Nicolas
+"""
+class Gate(object):
+	""" Classe Gate qui represante une porte logique
+
+	Deux entree A et B, et une sortie S
+	"""
+	def __init__(self, entranceA, entranceB):
+		"""	Constructeur, prend en param 2 bool"""
+		self.entranceA = entranceA
+		self.entranceB = entranceB
+
+	def __str__(self):
+		return "Entre A: %s, entranceB: %s" % (self.entranceA, self.entranceB)
+
+class ANDGate(Gate):
+	""" Classe de la porte AND"""
+	def __init__(self, entranceA, entranceB):
+		Gate.__init__(self, entranceA, entranceB)
+		self.sortieS = entranceA & entranceB
+
+	def __str__(self):
+		self.sortieS = self.entranceA & self.entranceB
+		return "Entre A: %s, entranceB: %s \nSortieS: %s" % (self.entranceA, self.entranceB, self.sortieS)
+
+	def __and__(self, other):
+		return (self.entranceA & self.entranceB) & (other.entranceA & other.entranceB)
---
+++ main.py
@@ -0,0 +1,6 @@
+import Gate
+
+porte = Gate.Gate(True, True)
+lol = Gate.Gate(True, True)
+print porte & lol
+print porte
