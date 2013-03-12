3ada85f15a17fa6867a9285742306a0b6f042f70
Jorjor70 <martin.jordy70@gmail.com>
2013-03-12 18:48:37 +0100

    Re-correction d'un gros bug

--- src/fonction.py
+++ src/fonction.py
@@ -328,7 +328,6 @@ def composition(l):
 			except Exception :
 				None
 		if(len(str(l[i]))==2 or len(str(l[i]))==3):
-			print l[i]
 			if(l[i]=="and" or l[i]=="or"):
 				if(l[i+1]=='not' and len(str(l[i+2]))==1):
 					porte.append(str(nombreInsertion-1)+","+l[i]+","+l[i+1]+" "+l[i+2])
@@ -342,13 +341,11 @@ def composition(l):
 						porte.append(str(nombreInsertion-1-exprPar)+","+l[i]+","+l[i+1])
 					nombreInsertion+=1
 				elif(len(str(l[i-1]))==1):
-					print"fuck"
 					if(str(l[i-2])=='or' or str(l[i-2])=='and'):
-						print "tu veux voir ma bite"
-						if(len(l[i+1])==1):
-							porte.append(str(nombreInsertion-1)+","+l[i]+","+l[i+1])
-						elif(len(l[i+1])>3):
+						if(len(str(l[i+1]))>3):
 							porte.append(str(nombreInsertion-1)+","+l[i]+","+str(nombreInsertion+1))
+						elif(len(str(l[i+1]))==1):
+							porte.append(str(nombreInsertion-1)+","+l[i]+","+l[i+1])
 						nombreInsertion+=1
 				elif(len(str(l[i+1]))>1):
 					porte.append(str(nombreInsertion-1)+","+l[i]+","+str(nombreInsertion+1))
@@ -418,7 +415,7 @@ if __name__=="__main__":
 		#expr = 'a and b or ((a and b) or (c and d))'
 		#expr = '(((a and b) and (b and c)) or (b and c)) and ((a and b) and (b and c))'
 		#expr = '(((a and b) and v) or c)'
-		expr = '(((a and b) or c) and d) or a and c or c'
+		expr = '((a and b) and c) or (k and g)'
 		#expr = '(a and v) and c or c and ((a and b) and (b and c))' a gerer les priorités entre parenthèses.
 		#expr = 'a and b and c and d'
 		#expr = '(a and not r) and b'
